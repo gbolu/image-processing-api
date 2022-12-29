b43580bc37ecc415442735b5f7160c59364755df
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2022-12-29 18:26:37 +0100

    chore: complete error handler type info

--- src/util/middlewares/globalErrorHandler.ts
+++ src/util/middlewares/globalErrorHandler.ts
@@ -1,20 +1,22 @@
 import Environment from '../../config/env'
 import debugResponse from '../responseHandlers/debugResponse'
 import { AppError } from '../appError'
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import sendProductionErrors from '../responseHandlers/sendProductionError'
 
-export default function globalErrorHandlerMiddleware(
+export default async function globalErrorHandlerMiddleware(
   err: AppError,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) {
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || false
+  const error = err as AppError
+  error.statusCode = err.statusCode || 500
+  error.status = err.status || false
 
   if (Environment.Server.Environment === 'production') {
-    return sendProductionErrors(err, res)
+    return sendProductionErrors(error, res)
   }
 
-  return debugResponse(err, res)
+  return debugResponse(error, res)
 }
